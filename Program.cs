a043fa7bcca5dd38f13aa72c6239143190b959b1
Udassar <udginsmail@gmail.com>
2023-06-20 11:34:11 +0300

    Решил второе задание

--- Program.cs
+++ Program.cs
@@ -13,19 +13,44 @@ Console.WriteLine($"Вторая цифра числа {num} - это {u}");*/
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine()!);
+
+if (num <= 99)
+{
+    Console.WriteLine("Not found");
+}
+else if (num > 99 && num <= 999)
+{
+    int num3 = num % 10;
+    Console.WriteLine($"Третья ццифра {num3}");
+}
+else if (num > 999 && num <= 9999)
+{
+    int num3 = num % 10 / 10;
+    Console.WriteLine($"Третья ццифра {num3}");
+}
+else if (num > 9999 && num <= 99999)
+{
+    int num3 = num % 10 / 100;
+    Console.WriteLine($"Третья ццифра {num3}");
+}
+
+
+
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.(использовать И и ИЛИ)
 // 6 -> да
 // 7 -> да
 // 1 -> нет
 
-Console.WriteLine("Введите номер дня недели");
-int num = Convert.ToInt32(Console.ReadLine()!);
+// Console.WriteLine("Введите номер дня недели");
+// int num = Convert.ToInt32(Console.ReadLine()!);
 
-if (num == 6 || num == 7)
-{
-    Console.WriteLine("Это выходной");
-}
-else
-{
-    Console.WriteLine("Не выходной");
-}
+// if (num == 6 || num == 7)
+// {
+//     Console.WriteLine("Это выходной");
+// }
+// else
+// {
+//     Console.WriteLine("Не выходной");
+// }
